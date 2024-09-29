f7b85c910e2301caa4804524c255cdc5ca723434
vveell32 <vveell@gmail.com>
2024-09-29 19:23:01 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+Name = "Valera"
+print(Name[0:3])
+print(Name)
+Age = 36
+print(Age)
+New_age = Age + 1
+print(New_age)
+Is_student = True
+print(Is_student)
+print(id(Is_student),"Welcome_to_Python")
